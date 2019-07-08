d4b7c55676bf08a7a093373433d5d669cbf57bcd
Breyanna Vass <breyannavass@gmail.com>
2019-07-08 13:09:35 -0400

    Updating Input Abilities

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends React.Component {
 
   getWeather = async (e) => {
     e.preventDefault();
+    const city = e.target.elements.name.city.value;
+    const country = e.target.elements.name.country.value;
     const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log('data')
