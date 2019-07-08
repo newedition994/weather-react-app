5297e926bb1afa853b4de13f92124fb011856b0a
Breyanna Vass <breyannavass@gmail.com>
2019-07-08 13:01:49 -0400

    Rearranging api details

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/weather";
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log('data')
+  }
+
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Currently</h1>
+            </div>
+        )
+    }
+}
+
+export default Weather;
