a46176fd6e684f7e17261c1c69e7c62c1cdd7013
Breyanna Vass <breyannavass@gmail.com>
2019-07-15 18:55:40 -0400

    Updating display for Weather component

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,11 +4,10 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
+                <h2>Location: {this.props.city}, {this.props.country} </h2>
                 <h1>Tempeture: {this.props.temperature}</h1>
-                <h2>City: {this.props.city}</h2>
-                <h2>Country: {this.props.country}</h2>
                 <p>Humidity: {this.props.humidity}</p>
-                <p>Decsription: {this.props.description}</p>
+                <p>Conditions: {this.props.description}</p>
             </div>
         )
     }
