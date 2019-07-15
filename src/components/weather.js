0aa6e3d6d5a7858095a7f416c1ff271e020e1536
Breyanna Vass <breyannavass@gmail.com>
2019-07-15 19:01:28 -0400

    && statements

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,10 +4,11 @@ class Weather extends React.Component {
     render() {
         return (
             <div>
-                <h2>Location: {this.props.city}, {this.props.country} </h2>
-                <h1>Tempeture: {this.props.temperature}</h1>
-                <p>Humidity: {this.props.humidity}</p>
-                <p>Conditions: {this.props.description}</p>
+
+                {this.props.city && this.props.country && <h2>Location: {this.props.city}, {this.props.country} </h2>}
+                {this.props.tempature && <h1>Temperature: {this.props.temperature}</h1>}
+                {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
             </div>
         )
     }
